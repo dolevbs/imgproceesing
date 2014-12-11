e2f0e72cbddade5d7f72291c2816650b661b4d32
Barak Hirsch <barak678@gmail.com>
2014-12-11 11:20:15 +0200

    Start to implement the filter grid

--- ImageProcessing/Grayscale/InputFilterDialog.xaml.cs
+++ ImageProcessing/Grayscale/InputFilterDialog.xaml.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace Grayscale
 {
     /// <summary>
     /// Interaction logic for InputFilterDialog.xaml
     /// </summary>
-    public partial class InputFilterDialog : Window
+    public partial class InputFilterDialog
     {
         public InputFilterDialog()
         {
@@ -25,5 +18,41 @@ namespace Grayscale
         }
 
         public double[,] FilterMatrix { get; set; }
+
+        private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var tag = e.AddedItems.OfType<ComboBoxItem>().Single().Tag.ToString();
+
+            var selectedSize = int.Parse(tag);
+
+            if (FilterGrid == null) return;
+
+            FilterGrid.Children.Clear();
+
+            FilterGrid.RowDefinitions.Clear();
+            FilterGrid.ColumnDefinitions.Clear();
+
+            for (int i = 0; i < selectedSize; i++)
+            {
+                FilterGrid.RowDefinitions.Add(new RowDefinition());
+                FilterGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 0; i < selectedSize; i++)
+            {
+                for (int j = 0; j < selectedSize; j++)
+                {
+                    var cell = new RadioButton
+                    {
+                        Style = Resources["ToggleRadio"] as Style
+                    };
+
+                    Grid.SetRow(cell, i);
+                    Grid.SetColumn(cell, j);
+
+                    FilterGrid.Children.Add(cell);
+                }
+            }
+        }
     }
 }
